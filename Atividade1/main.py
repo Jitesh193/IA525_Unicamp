15c1df25d5b42a5196f77889b17f3e242d3ddb9d
Jitesh Vassaram <jitesh.amv@gmail.com>
2024-03-13 15:20:29 -0300

    Adicao dos parametros de entrada e o comeco do Monte Carlo

--- Atividade1/main.py
+++ Atividade1/main.py
@@ -13,10 +13,50 @@ https://medium.com/@whystudying/monte-carlo-simulation-with-python-13e09731d500
 
 # import numpy as np
 from random import uniform
+import matplotlib.pyplot as plt
 
 # Definindo as entradas do Simulador Monte Carlo
+sim = 1000  # Numero de simulacoes
+quad = 0    # Numero de pontos dentro do quadrado
+pol = 0     # Numero de pontos dentro do poligono
+
 
 # Definindo os pontos do poligono
 
+# n = int(input('Digite a quantidade de vertices: '))
+# vert_x = []
+# vert_y = []
+#
+# for c in range(1, n+1):
+#
+#     x = float(input(f'Digite o {c}o vertice em x: '))
+#     y = float(input(f'Digite o {c}o vertice em y: '))
+#
+#     vert_x.append(x)
+#     vert_y.append(y)
+#
+# print(vert_x)
+# print(vert_y)
+vert_x = (0.2, 0.8, 0.2)
+vert_y = (0.3, 0.3, 0.8)
 
+
+plt.plot(vert_x, vert_y, 'k--')
+plt.plot(vert_x,vert_y, 'go')
+plt.axis([0, 1, 0, 1])
+plt.grid(True)
+plt.show()
+
+
+# Monte Carlo Simulation
+l_xs = []
+l_ys = []
+
+for i in range(sim):
+
+    x = uniform(0, 1)
+    l_xs.append(x)
+
+    y = uniform(0, 1)
+    l_ys.append(y)
 
