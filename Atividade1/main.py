075ae05e2df330c8482e12b0158a8de6f85e0495
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-03-13 10:41:10 -0300

    Incluindo alguns comentarios no main.py

--- Atividade1/main.py
+++ Atividade1/main.py
@@ -1,1 +1,22 @@
+""""
+
+Será utilizado simulacao de Monte Carlo para gerar os pontos necessários!
+
+Para isso, as entradas são os pontos sorteados uniformemente X e Y entre [0,1] e o número de simulações que serão feitas
+
+Outra entrada seria os pontos do polígono que se deseja calcular a área (será utilizado no 2o loop)
+
+https://medium.com/@whystudying/monte-carlo-simulation-with-python-13e09731d500
+
+"""
+
+
+# import numpy as np
+from random import uniform
+
+# Definindo as entradas do Simulador Monte Carlo
+
+# Definindo os pontos do poligono
+
+
 
