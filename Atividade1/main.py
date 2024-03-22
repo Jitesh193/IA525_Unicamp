2bd0b5fab5ccf8cd71da66ae9da654d029218bd0
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-03-22 12:06:34 -0300

    programa funcionando e comentado

--- Atividade1/main.py
+++ Atividade1/main.py
@@ -1,17 +1,10 @@
 """"
-
-Será utilizado simulacao de Monte Carlo para gerar os pontos necessários!
-
-Para isso, as entradas são os pontos sorteados uniformemente X e Y entre [0,1] e o número de simulações que serão feitas
-
-Outra entrada seria os pontos do polígono que se deseja calcular a área (será utilizado no 2o loop)
-
-https://medium.com/@whystudying/monte-carlo-simulation-with-python-13e09731d500
+Atividade 1 - Area de um Poligono Convexo - IA525
+Nome: Jitesh Ashok Manilal Vassaram
+RA: 175867
 
 """
 
-
-# import numpy as np
 from random import uniform
 import matplotlib.pyplot as plt
 from Atividade1 import p_inside
@@ -29,12 +22,17 @@ pol = 0     # Numero de pontos dentro do poligono
 # vert_y = (0.1, 0.8, 0.6, 0.3)
 
 # Vertices do poligono especial
-vert_x = (0.623, 0.9279, 0.6410, 0.2153, 0.0119)
-vert_y = (0.9478, 0.9185, 0.1372, 0.1420, 0.4632)
+# vert_x = (0.623, 0.9279, 0.6410, 0.2153, 0.0119)
+# vert_y = (0.9478, 0.9185, 0.1372, 0.1420, 0.4632)
+
+# Vertices do poligono nao convexo
+vert_x = (0.1, 0.3, 0.6, 0.8, 0.4)
+vert_y = (0.9, 0.7, 0.8, 0.4, 0.1)
 
 
-n = len(vert_x)
-# Monte Carlo Simulation
+n = len(vert_x)     # Contagem do numero de vertices/arestas
+
+# Simulacao Monte Carlo para gerar os pontos "x" e "y" simulados
 l_xs = []
 l_ys = []
 
@@ -57,6 +55,8 @@ for j in range(sim):
 
     d = p_inside.dotprod(vert_x, vert_y, l_xs[j], l_ys[j], n)
 
+    # Caso o ponto esteja dentro do poligono (d == True), soma-se +1 na variavel "pol" e armazena-se o ponto em uma
+    # lista para plotagem das figuras
     if d:
         pol += 1
         pontos_polx.append(l_xs[j])
@@ -65,11 +65,13 @@ for j in range(sim):
         pontos_quadx.append(l_xs[j])
         pontos_quady.append(l_ys[j])
 
+
+# Printa a quantidade de pontos dentro do poligono e a area estimada
 print(f'O numero de pontos dentro do poligono é de: {pol}')
 print(f'O numero de pontos dentro do quadrado é de: {sim}')
 print(f'A area do poligono sera: {pol/sim:.4f}')
 
-
+# Inclusao do ultimo ponto para a geracao das figuras
 lista_x = list(vert_x)
 lista_y = list(vert_y)
 lista_x.append(vert_x[0])
@@ -78,13 +80,13 @@ lista_y.append(vert_y[0])
 vert_x = tuple(lista_x)
 vert_y = tuple(lista_y)
 
-
+# Figuras
 plt.axhline(y=1, color='red', linestyle='--')
 plt.axhline(y=0, color='red', linestyle='--')
 plt.axvline(x=1, color='red', linestyle='--')
 plt.axvline(x=0, color='red', linestyle='--')
 plt.plot(vert_x, vert_y, 'k--')
-plt.plot(vert_x, vert_y, 'go')
+plt.plot(vert_x, vert_y, 'go',ms=10)
 plt.plot(pontos_polx, pontos_poly, 'bo', ms=2, label='Pontos dentro do Poligono')
 plt.plot(pontos_quadx, pontos_quady, 'ro', ms=2, label='Pontos dentro do Quadrado')
 plt.axis([-0.125, 1.125, -0.125, 1.125])
--- Atividade1/p_inside.py
+++ Atividade1/p_inside.py
@@ -7,7 +7,7 @@ def dotprod(x, y, xp, yp, n):
     yp: vertice 'y' do ponto que se deseja avaliar
     n: numero de vertices do poligono
     """
-
+    # Inicializacao das variaveis
     dpos = 0
     dneg = 0
     dentro = False
@@ -15,11 +15,14 @@ def dotprod(x, y, xp, yp, n):
     x0 = x[0]
     y0 = y[0]
 
+    # Avaliacao para ver se o ponto esta dentro ou nao do poligono
     for i in range(1, n + 1):
 
+        # Caso esteja no ultimo ponto, a proxima aresta a ser avaliada eh a que liga o ultimo com o primeiro vertice
         if i == n:
             x1 = x[0]
             y1 = y[0]
+        # Caso contrario, a aresta eh o ponto atual (x0,y0) com o proximo ponto da ordem (x1,y1)
         else:
             x1 = x[i]
             y1 = y[i]
@@ -27,16 +30,22 @@ def dotprod(x, y, xp, yp, n):
         dprod = (xp - x0)*(y1 - y0) - (yp - y0)*(x1 - x0)   # Produto interno entre o vetor vertice-ponto e o vetor
                                                             # normal da reta formada pelos vertices do poligono
 
+        # Caso o produto interno >= 0 -> o vetor vertice-ponto esta na mesma direcao do vetor normal da aresta
         if dprod > 0:
             dpos += 1
         elif dprod == 0:
             dpos += 1
+
+        # Caso contrario, o vetor vertice-ponto esta no lado oposto do vetor normal da aresta
         else:
             dneg += 1
 
         x0 = x1
         y0 = y1
 
+    # Se houver algum ponto que esta na direcao oposta do vetor normal da aresta (dneg > 0), isso indica que o
+    # ponto esta fora do poligono, logo a variavel "dentro" eh False. Caso contrario, isto eh, "dneg == 0" indica que o
+    # ponto esta dentro do poligono e a variavel "dentro" eh True
     if dpos > 0 and dneg == 0:
         dentro = True
     elif dneg > 0:
