62b304702526b9b8820147be17dcadcfef9cccaf
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-04-08 17:08:23 -0300

    entendimento da recursao

--- Atividade2/main.py
+++ Atividade2/main.py
@@ -15,14 +15,22 @@
 
 def max_profit_cutting_recursive(n, l):
     if n == 0:
-        return 0
+        return 0, []
+    
     max_profit = float('-inf')
-    for i in range(1, n + 1):
-        max_profit = max(max_profit, l[i - 1] + max_profit_cutting_recursive(n - i, l))
-    return max_profit
+    best_cut = None
 
+    for i in range(1, n + 1):
+        profit, cuts = max_profit_cutting_recursive(n - i, l)
+        if l[i - 1] + profit > max_profit:
+            max_profit = l[i - 1] + profit
+            best_cut = [i] + cuts
+
+    return max_profit, best_cut
 
 # Exemplo de uso
 n = 4
 l = [4, 8, 13, 15]
-print("Lucro máximo:", max_profit_cutting_recursive(n, l))
+profit, cuts = max_profit_cutting_recursive(n, l)
+print("Lucro máximo:", profit)
+print("Cortes:", cuts)
