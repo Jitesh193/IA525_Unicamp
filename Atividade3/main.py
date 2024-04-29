1eb4f52b9be5057183386e939814b8c3b7b55903
Jitesh Ashok <jitesh.ashok@icloud.com>
2024-04-29 12:04:41 -0300

    adicao do sinal senoidal com ruido gaussiano

--- Atividade3/main.py
+++ Atividade3/main.py
@@ -5,9 +5,10 @@ RA: 175867
 """
 
 import numpy as np
-# import cvxpy 
+import matplotlib.pyplot as plt
+import cvxpy 
 
-
+np.random.seed(42)
 n = 4
 delta = np.zeros((n,n))
 
@@ -28,3 +29,31 @@ for i in range(n):
            delta[i,j] = 0
 
 print(f'A matriz Delta é: \n {delta}')
+
+"Numero de pontos do Sinal Senoidal"
+num_pontos = 1000
+
+"Sinal Senoidal"
+t = np.linspace(0,5, num_pontos)
+f = 1
+sen = np.sin(2*np.pi*f*t)
+
+"Ruido Gaussiano"
+ruido = np.random.normal(0,np.sqrt(0.01), num_pontos)
+
+"Sinal Final"
+x_c = sen + ruido
+
+"Plot do sinal"
+plt.figure(figsize=(10, 6))
+plt.plot(t, sen, label='Sinal Senoidal')
+plt.plot(t, x_c, label='Sinal com Ruído Gaussiano')
+plt.xlabel('Tempo')
+plt.ylabel('Amplitude')
+plt.title('Sinal Senoidal com Ruído Gaussiano')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+"Utilizacao da biblioteca 'cvxpy' "
+
