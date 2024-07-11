33b1b0f9d2324d8ca1281149574c83f20e110a42
Jitesh Vassaram <jitesh.amv@gmail.com>
2024-07-11 11:59:49 -0300

    codigo senha.m feito!

--- Atividade7/senha.m
+++ Atividade7/senha.m
@@ -20,46 +20,61 @@ cvx_solver mosek
     variable x(10,3) binary % Variaveis de decisao
 
     subject to
-        % Percorre cada linha da matriz de dicas
-        for i=1:3
-            d = dicas(i,1:3);
-            % Isso garante que os elementos da dica serao possibilidades na
-            % matriz - agora tem que indicar se está repetido ou não
-            for j = 1:length(d)
-                linha = d(j);
-                coluna = j;
-                x(linha,coluna) == 1;
-            end
-
-
-
-
-        end
-
+        % Restricoes da matriz x
         for i=1:10
-            sum(x(i,:)) == 1;
+            sum(x(i,:)) <= 1;   % Cada digito aparece no máximo 1 vez na senha
         end
         for i=1:3
-            sum(x(:,i)) == 1;
+            sum(x(:,i)) == 1;   % cada posicao deve ter apenas 1 digito
         end
+
+        % Restricoes baseadas nas dicas dadas no exemplo-base
+        for i=1:5
+            dica = dicas(i,1:3);
+            pos_correto = dicas(i,4);   % Numero de digitos corretos na pos. correta
+            pos_errado = dicas(i,5);    % Numero de digitos corretos na pos. errada
+
+            contagem_corr = 0;  % contagem de digitos corretos na pos. correta
+            
+            for j = 1:3
+                if dica(j) > 0
+                    cor = dica(j);
+                    contagem_corr = contagem_corr + x(cor+1, j);
+                end
+            end
+            contagem_corr == pos_correto;   % Assegura que o modelo siga o numero de digitos corretos na pos. correta
+                                            % dado pela dica 
+
+            
+            contagem_err = 0;  % contagem de digitos corretos na pos. errada
+            for j = 1:3
+                if dica(j) > 0
+                    err = dica(j);
+                    for l = 1:3
+                        if l ~= j
+                            contagem_err = contagem_err + x(err+1, l);
+                        end
+                    end
+                end
+            end
+            contagem_err == pos_errado;     % Assegura que o modelo siga o numero de digitos corretos na pos. errado
+                                            % dado pela dica 
+        end
+    
+
 cvx_end
 
+% Extracao da solucao
+sol = zeros(1,3);
+x = full(x);
+% j=1;
+for i=1:10
+    for j=1:3
+        if(x(i,j)==1)
+            sol(1,j) = i-1;
+        end
+    end
+end
 
-%%
-
-% % Vetor com os elementos
-% vetor = [7, 2, 5];
-% 
-% % Inicializa a matriz de zeros 10x3
-% matriz = zeros(10, 3);
-% 
-% % Atualiza a matriz na posição correspondente
-% for i = 1:length(vetor)
-%     linha = vetor(i) + 1; % Adiciona 1 porque as linhas representam os números 0-9, então 7 -> 8
-%     coluna = i;
-%     matriz(linha, coluna) = 1;
-% end
-% 
-% % Exibir a matriz
-% disp('Matriz atualizada:');
-% disp(matriz);
+disp('A senha, baseada nas dicas dadas é:')
+disp(sol)
